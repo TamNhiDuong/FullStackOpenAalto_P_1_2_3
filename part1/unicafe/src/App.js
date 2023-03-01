3b25e3cdfca500312e78ccfab063a8e5bad020c5
TamNhiDuong <yanxinyee@gmail.com>
2023-03-01 21:11:07 +0200

    Exc 1.8 separate Statistics component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,40 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad, total }) => {
+  return (
+    <>
+      <h1>statistics</h1>
+
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <p style={{ marginRight: 10 }}>good</p>
+        <p>{good}</p>
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <p style={{ marginRight: 10 }}>neutral</p>
+        <p>{neutral}</p>
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <p style={{ marginRight: 10 }}>bad</p>
+        <p>{bad}</p>
+      </div>
+
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <p style={{ marginRight: 10 }}>all</p>
+        <p>{total}</p>
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <p style={{ marginRight: 10 }}>average</p>
+        {total !== 0 && <p>{(good + neutral * 0 - bad) / total}</p>}
+      </div>
+
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <p style={{ marginRight: 10 }}>positive</p>
+        {total !== 0 && <p>{good * 100 / (total) + ' %'}</p>}
+      </div>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -27,34 +62,7 @@ const App = () => {
       <button onClick={addNeutral}>neutral</button>
       <button onClick={addBad}>bad</button>
 
-      <h1>statistics</h1>
-
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <p style={{ marginRight: 10 }}>good</p>
-        <p>{good}</p>
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <p style={{ marginRight: 10 }}>neutral</p>
-        <p>{neutral}</p>
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <p style={{ marginRight: 10 }}>bad</p>
-        <p>{bad}</p>
-      </div>
-      
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <p style={{ marginRight: 10 }}>all</p>
-        <p>{total}</p>
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <p style={{ marginRight: 10 }}>average</p>
-        {total !== 0 && <p>{(good + neutral * 0 - bad) / total}</p>}
-      </div>
-
-      <div style={{display: 'flex', flexDirection: 'row'}}>
-        <p style={{marginRight: 10}}>positive</p>
-        {total !== 0 && <p>{good*100/(total) + ' %'}</p>}
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} />
 
 
     </div>
