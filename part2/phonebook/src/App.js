bee70ff7b9f162bb84b18efb25f385b2bd8ae379
TamNhiDuong <yanxinyee@gmail.com>
2023-03-07 22:23:52 +0200

    Exc 2.6 phonebook form

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+const Person = (props) => {
+  const { person } = props
+  return (
+    <p>{person.name}</p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const addNote = (e) => {
+    e.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNote}>
+        <div>
+          name: <input onChange={handleChange} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <Person person={person} key={person.name} />)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
