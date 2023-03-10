e25e187f556b46870753e1becf1a743655bfa870
TamNhiDuong <yanxinyee@gmail.com>
2023-03-10 21:55:46 +0200

    Exc 10 Refactor

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,6 +8,35 @@ const Person = (props) => {
   )
 }
 
+const Form = (props) => {
+  const { addNote, handleChange, newName, handleNumberChange, newNumber } = props
+  return (
+    <form onSubmit={addNote}>
+      <div>name: <input onChange={handleChange} value={newName} /></div>
+      <div>number: <input onChange={handleNumberChange} value={newNumber} /></div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Filter = (props) => {
+  const { handleFilterChange, filterWord } = props
+  return (
+    <div>filter show with: <input onChange={handleFilterChange} value={filterWord} /></div>
+  )
+}
+
+const ContactList = (props) => {
+  const { filteredList } = props
+  return (
+    <ul>
+      {filteredList.map(person => <Person person={person} key={person.name} />)}
+    </ul>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -53,20 +82,20 @@ const App = () => {
     <div>
       <h1>Phonebook</h1>
 
-      <div>filter show with: <input onChange={handleFilterChange} value={filterWord} /></div>
+      <Filter
+        handleFilterChange={handleFilterChange}
+        filterWord={filterWord} />
 
-      <form onSubmit={addNote}>
-        <h2>Add a new</h2>
-        <div>name: <input onChange={handleChange} value={newName} /></div>
-        <div>number: <input onChange={handleNumberChange} value={newNumber} /></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {filteredList.map(person => <Person person={person} key={person.name} />)}
-      </ul>
+      <h3>Add a new</h3>
+      <Form
+        addNote={addNote}
+        handleChange={handleChange}
+        newName={newName}
+        handleNumberChange={handleNumberChange}
+        newNumber={newNumber} />
+
+      <h3>Numbers</h3>
+      <ContactList filteredList={filteredList} />
     </div>
   )
 }
